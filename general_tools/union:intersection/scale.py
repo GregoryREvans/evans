08452f019ca1d3bb1b380060470dcd81b8ebd4d5
GregoryREvans <gregoryrowlandevans@gmail.com>
2018-12-03 18:47:37 -0500

    Added scale function.

---
+++ general_tools/union:intersection/scale.py
@@ -0,0 +1,11 @@
+def scale(start, step, reps):
+    list = [start, ]
+    for _ in range(reps):
+        new = [list[-1] + step]
+        list.extend(new)
+    return list
+
+scale_1 = scale(0, 2, 5)
+scale_2 = scale(-2, 1.5, 7)
+print(scale_1)
+print(scale_2)
