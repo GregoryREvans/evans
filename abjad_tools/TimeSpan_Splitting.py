4979f827dfc5dc78fd3aa5f4ba1a3b8a52f2dbe0
GregoryREvans <gregoryrowlandevans@gmail.com>
2018-12-22 21:37:08 -0500

    New beaming_algorithm.

---
+++ abjad_tools/TimeSpan_Splitting.py
@@ -0,0 +1,15 @@
+import abjad
+
+time_signatures = [
+    abjad.TimeSignature(pair) for pair in [
+        (4, 4), (4, 4), (4, 4), (4, 4), (4, 4),
+        (4, 4), (4, 4), (4, 4), (4, 4), (4, 4),
+        (4, 4), (4, 4), (4, 4), (4, 4), (4, 4),
+        (4, 4), (4, 4), (4, 4), (4, 4), (4, 4),
+        (4, 4), (4, 4), (4, 4), (4, 4), (4, 4),
+    ]
+]
+
+bounds = abjad.mathtools.cumulative_sums([_.duration for _ in time_signatures])
+
+print(bounds)
