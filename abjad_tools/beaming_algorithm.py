27f2c680571422b3532d8e797fbfff8b2fd29c37
GregoryREvans <gregoryrowlandevans@gmail.com>
2018-12-24 18:00:51 -0500

    New beaming algorithm.

--- abjad_tools/beaming_algorithm.py
+++ abjad_tools/beaming_algorithm.py
@@ -4,19 +4,4 @@ for voice in abjad.select(score).components(abjad.Voice):
             beam_each_division=False,
             )
         specifier(run)
-    for leaf in abjad.select(voice).leaves():
-        if leaf.written_duration <= abjad.Duration(7, 32):
-            previous_leaf = abjad.inspect(leaf).leaf(-1)
-            next_leaf = abjad.inspect(leaf).leaf(1)
-            prev_dur = 100
-            next_dur = 100
-            if hasattr(previous_leaf, 'written_duration'):
-                prev_dur = previous_leaf.written_duration
-            if hasattr(next_leaf, 'written_duration'):
-                next_dur = next_leaf.written_duration
-            if all(dur >= abjad.Duration(1, 4) for dur in (prev_dur, next_dur)):
-                continue
-            if prev_dur >= abjad.Duration(1, 4):
-                abjad.attach(abjad.StartBeam(), leaf)
-            if next_dur >= abjad.Duration(1, 4):
-                abjad.attach(abjad.StopBeam(), leaf)
+    abjad.beam(voice[:], beam_lone_notes=False, beam_rests=False,)
