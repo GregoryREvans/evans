5c55e9d5d6aed7c150c4b5c2f83ba57f1f9e9749
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-03-14 15:35:11 -0400

    Added step_list to random_walk.

--- general_tools/random_walk.py
+++ general_tools/random_walk.py
@@ -1,13 +1,20 @@
 from random import random
 from random import seed
 from evans.general_tools.reduce_mod import reduceMod
+from evans.general_tools.cyc import cyc
 
-def randomWalk(random_seed, length, mapped_list):
+def randomWalk(random_seed, length, step_list, mapped_list):
     seed(random_seed)
+    if step_list != None:
+        step = cyc(step_list)
     walk = []
     walk.append(-1 if random() < 0.5 else 1)
     for i in range(1, length):
-        movement = -1 if random() < 0.5 else 1
+        if step_list != None:
+            next_step = next(step)
+            movement = -next_step if random() < 0.5 else next_step
+        else:
+            movement = -1 if random() < 0.5 else 1
         value = walk[i-1] + movement
         walk.append(value)
     input_list = mapped_list
