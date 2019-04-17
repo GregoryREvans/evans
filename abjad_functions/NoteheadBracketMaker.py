4fda72f2ee87430f0acd0e3d37ec440454c1c3cb
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-04-17 00:11:36 -0400

    Testing Lilypond Literal.

--- abjad_functions/NoteheadBracketMaker.py
+++ abjad_functions/NoteheadBracketMaker.py
@@ -11,16 +11,16 @@ class NoteheadBracketMaker:
             denominator = written_duration.denominator
             imp_num, imp_den = tuplet.implied_prolation.pair
             notehead_wrapper = denominator * imp_num
-            abjad.tweak(tuplet).TupletNumber.text = f'(tuplet-number::append-note-wrapper(tuplet-number::non-default-tuplet-fraction-text {imp_den} {imp_num}) "{notehead_wrapper}")'
+            abjad.tweak(tuplet).TupletNumber.text = abjad.LilyPondLiteral(f'(tuplet-number::append-note-wrapper(tuplet-number::non-default-tuplet-fraction-text {imp_den} {imp_num}) "{notehead_wrapper}")')
             # literal = abjad.LilyPondLiteral(r'\tweak TupletNumber.text #(tuplet-number::append-note-wrapper' + f'(tuplet-number::non-default-tuplet-fraction-text {imp_den} {imp_num}) "{notehead_wrapper}")', format_slot='absolute_before')
             # abjad.attach(literal, tuplet)
         return selections
 
-# ###DEMO###
-# tuplet = abjad.Tuplet((3, 2), "c'8 d'8")
-# tuplet_2 = abjad.Tuplet((2, 3), components=[abjad.Note(0, (1, 2)), tuplet])
-# staff = abjad.Staff()
-# staff.append(tuplet_2)
-# new_brackets = NoteheadBracketMaker()
-# new_brackets(staff)
-# abjad.show(staff)
+###DEMO###
+tuplet = abjad.Tuplet((3, 2), "c'8 d'8")
+tuplet_2 = abjad.Tuplet((2, 3), components=[abjad.Note(0, (1, 2)), tuplet])
+staff = abjad.Staff()
+staff.append(tuplet_2)
+new_brackets = NoteheadBracketMaker()
+new_brackets(staff)
+abjad.show(staff)
