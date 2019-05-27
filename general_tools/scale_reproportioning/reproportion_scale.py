e0e48a0c697b933ecfc85349f068f7fcdd07d350
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-05-27 18:17:32 -0400

    Scaling project.

--- general_tools/scale_reproportioning/reproportion_scale.py
+++ general_tools/scale_reproportioning/reproportion_scale.py
@@ -1,49 +1,43 @@
-def reproportion_scale(base, partial_limit):
+import abjad
+
+def reproportion_scale(base, limit):
     step = base / 10.0
-    end = partial_limit + 1
+    end = limit + 1
     scale = [_ for _ in range(2, end)]
     new_scale = [_ * step for _ in scale]
-    print(new_scale)
     return new_scale
 
 def reproportion_harmonics(fund, scale):
     calculated_series = [_ * fund for _ in scale]
     final_series = [fund,]
     final_series.extend(calculated_series)
-    print(final_series)
     return final_series
 
+def _to_nearest_eighth_tone(number):
+    number = round(float(number) * 4) / 4
+    div, mod = divmod(number, 1)
+    if mod == 0.75:
+        div += 0.75 # used to be 1
+    elif mod == 0.5:
+        div += 0.5
+    elif mod == 0.25: # new
+        div += 0.25 # new
+    return abjad.mathtools.integer_equivalent_number_to_integer(div)
+
+def reproportion_chromatic_decimals(base, root_int, scale_range, round=False):
+    base_converter = base / 10.0
+    octave = root_int + 12
+    converted_octave = octave * base_converter
+    collection = [root_int, ]
+    step = converted_octave / 12
+    for _ in range(scale_range):
+            collection.append(collection[-1] + step)
+    if round is True:
+        collection = [_to_nearest_eighth_tone(_) for _ in collection]
+    return collection
+
 ###DEMO###
-print(10)
-insert_scale = reproportion_scale(base=10, partial_limit=17)
-harmonics = reproportion_harmonics(20, insert_scale)
-print(11)
-insert_scale = reproportion_scale(base=11, partial_limit=17)
-harmonics = reproportion_harmonics(20, insert_scale)
-print(12)
-insert_scale = reproportion_scale(base=12, partial_limit=17)
-harmonics = reproportion_harmonics(20, insert_scale)
-print(13)
-insert_scale = reproportion_scale(base=13, partial_limit=17)
-harmonics = reproportion_harmonics(20, insert_scale)
-print(14)
-insert_scale = reproportion_scale(base=14, partial_limit=17)
-harmonics = reproportion_harmonics(20, insert_scale)
-print(15)
-insert_scale = reproportion_scale(base=15, partial_limit=17)
-harmonics = reproportion_harmonics(20, insert_scale)
-print(16)
-insert_scale = reproportion_scale(base=16, partial_limit=17)
-harmonics = reproportion_harmonics(20, insert_scale)
-print(17)
-insert_scale = reproportion_scale(base=17, partial_limit=17)
-harmonics = reproportion_harmonics(20, insert_scale)
-print(18)
-insert_scale = reproportion_scale(base=18, partial_limit=17)
-harmonics = reproportion_harmonics(20, insert_scale)
-print(19)
-insert_scale = reproportion_scale(base=19, partial_limit=17)
-harmonics = reproportion_harmonics(20, insert_scale)
-print(20)
-insert_scale = reproportion_scale(base=20, partial_limit=17)
-harmonics = reproportion_harmonics(20, insert_scale)
+# insert_scale = (reproportion_scale(base=10, limit=17))
+# print(insert_scale)
+# print(reproportion_harmonics(fund=20, scale=insert_scale))
+# print(reproportion_chromatic_decimals(base=10, root_int=0, scale_range=12, round=True))
