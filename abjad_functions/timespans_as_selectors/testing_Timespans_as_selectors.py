98d514699629ff877874a948dd6a46d2db7653ec
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-05-29 19:11:41 -0400

    Working on using timespans as selectors.

---
+++ abjad_functions/timespans_as_selectors/testing_Timespans_as_selectors.py
@@ -0,0 +1,29 @@
+import abjad
+
+clef = abjad.Clef('treble')
+
+staff = abjad.Staff("c'4 d' e' f'")
+
+abjad.attach(clef, staff[0])
+
+selection = abjad.select(staff[:2]).leaves(pitched=True)
+for note in selection:
+    abjad.override(note).note_head.color = 'red'
+
+target_timespan = abjad.Timespan(start_offset=(0, 8), stop_offset=(1, 2))
+
+# for tie in abjad.select(staff).logical_ties(pitched=True): ##THIS WORKS
+#     if abjad.inspect(tie).timespan().happens_during_timespan(target_timespan) is True:
+#         for note in abjad.select(tie).leaves():
+#             note.written_pitch = note.written_pitch + 2
+
+# bool = lambda x: abjad.inspect(x).timespan().happens_during_timespan(target_timespan)
+# print(bool(staff[0]))
+
+for group in abjad.select(staff).logical_ties(pitched=True).group_by(predicate = lambda x: abjad.inspect(x).timespan().happens_during_timespan(target_timespan)):
+    if abjad.inspect(group).timespan().happens_during_timespan(target_timespan) is True: ##THIE WORKS TOO, BUT IS IT FASTER?
+        for tie in abjad.select(group).logical_ties(pitched=True):
+            for leaf in abjad.select(tie).leaves():
+                leaf.written_pitch = leaf.written_pitch + 2
+
+abjad.f(staff)
