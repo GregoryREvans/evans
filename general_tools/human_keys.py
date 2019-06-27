1fc9c349bd0ce35eb346cd224079a87e62820b14
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-06-27 12:11:32 -0400

    Separated human sorting options.

--- general_tools/human_keys.py
+++ general_tools/human_keys.py
@@ -5,15 +5,8 @@ def to_digit(string):
     return int(string) if string.isdigit() else string
 
 
-def human_sorted_keys(pair):
-    key, timespan = pair
-    values = [to_digit(_) for _ in key.split()]
-    hashable_key = tuple(values)
-    return hashable_key
-
-
-# def human_sorted_keys(text):
-#     return [_to_digit(_) for _ in re.split(r"(\d+)", text)]
+def human_sorted_keys(text):
+    return [_to_digit(_) for _ in re.split(r"(\d+)", text)]
 
 
 ###DEMO###
