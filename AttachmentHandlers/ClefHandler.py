0e32797c7d42ede3e8b0150afeb1df96a7d2c684
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-07-09 16:13:33 -0400

    Updated ClefHandler.

--- AttachmentHandlers/ClefHandler.py
+++ AttachmentHandlers/ClefHandler.py
@@ -1,22 +1,5 @@
 import abjad
-
-clef_groups_up = {
-    "bass": ("bass", "tenorvarC", "treble"),
-    "tenor": ("tenorvarC", "treble", "treble^8"),
-    "alto": ("varC", "treble", "treble^8"),
-    "treble": ("treble", "treble^8", "treble^15"),
-}
-
-clef_groups_down = {
-    "bass": ("bass", "bass_8", "bass_15"),
-    "tenor": ("tenorvarC", "bass", "bass_8"),
-    "alto": ("varC", "bass", "bass_8"),
-    "treble": ("treble", "treble_8", "bass"),
-}
-
-default_clef_shelves_up = {"bass": 3, "tenor": 10, "alto": 13, "treble": 36}
-
-default_clef_shelves_down = {"bass": -28, "tenor": -21, "alto": -12, "treble": -5}
+from statistics import mean
 
 
 class ClefHandler:
@@ -26,7 +9,6 @@ class ClefHandler:
         clef_shelf=None,
         add_extended_clefs=False,
         ottava_shelf=None,
-        ottava_number=1,
         add_ottavas=False,
         extend_in_direction="up",
     ):
@@ -34,22 +16,33 @@ class ClefHandler:
         self.clef_shelf = clef_shelf
         self.add_extended_clefs = add_extended_clefs
         self.ottava_shelf = ottava_shelf
-        self.ottava_number = ottava_number
         self.add_ottavas = add_ottavas
         self.extend_in_direction = extend_in_direction
 
     def __call__(self, voice):
         self._add_clefs(voice)
-        if self.add_ottavas is not False:
-            self._add_ottavas(voice)
 
     def _extended_range_clefs(self, clef):
+        clef_groups_up = {
+            "bass": ("bass", "tenorvarC", "treble"),
+            "tenor": ("tenorvarC", "treble", "treble^8"),
+            "alto": ("varC", "treble", "treble^8"),
+            "treble": ("treble", "treble^8", "treble^15"),
+        }
+        clef_groups_down = {
+            "bass": ("bass", "bass_8", "bass_15"),
+            "tenor": ("tenorvarC", "bass", "bass_8"),
+            "alto": ("varC", "bass", "bass_8"),
+            "treble": ("treble", "treble_8", "bass"),
+        }
         if self.extend_in_direction == "down":
             return clef_groups_down[clef]
         else:
             return clef_groups_up[clef]
 
     def _extended_range_ottavas(self, clef):
+        default_clef_shelves_up = {"bass": 3, "tenor": 10, "tenorvarC": 10, "alto": 13, "varC": 13, "treble": 24} #in some cases treble might be 36?
+        default_clef_shelves_down = {"bass": -28, "tenor": -21, "tenorvarC": -21, "alto": -12, "varC": -12, "treble": -5}
         if self.extend_in_direction == "down":
             return default_clef_shelves_down[clef]
         else:
@@ -58,36 +51,91 @@ class ClefHandler:
     def _add_clefs(self, voice):
         clef = self.clef
         if clef is not None:
+            clef_list = [abjad.Clef(self._extended_range_clefs(clef)[0])]
+            abjad.attach(clef_list[0], voice[0])
             if self.add_extended_clefs is True:
-                # clefs = _extended_range_clefs(clef)
-                for run in abjad.select(voice).runs():  # for now
-                    clef = abjad.Clef().from_selection(run)
-                    abjad.attach(clef, run[0])
+                clefs = self._extended_range_clefs(clef)
+                for run in abjad.select(voice).runs():
+                    ties = abjad.select(run).logical_ties(pitched=True)
+                    pitches = []
+                    for tie in ties:
+                        for pitch in abjad.inspect(tie[0]).pitches():
+                            pitches.append(pitch.number)
+                    average = mean(pitches)
+                    if average > self._extended_range_ottavas(clefs[0]):
+                        if average > self._extended_range_ottavas(clefs[1]):
+                            clef = abjad.Clef(clefs[2])
+                            if clef != clef_list[-1]:
+                                abjad.attach(clef, run[0])
+                                clef_list.append(clef)
+                            else:
+                                pass
+                        else:
+                            clef = abjad.Clef(clefs[1])
+                            if clef != clef_list[-1]:
+                                abjad.attach(clef, run[0])
+                                clef_list.append(clef)
+                            else:
+                                pass
+                    else:
+                        clef = abjad.Clef(clefs[0])
+                        if clef != clef_list[-1]:
+                            abjad.attach(clef, run[0])
+                            clef_list.append(clef)
+                        else:
+                            pass
+                    self._add_ottavas(run, clef.name)
+                clef_list.append(clef)
             else:
-                clef = abjad.Clef(self.clef)
+                converted_clef = self._extended_range_clefs(clef)[0]
+                clef = abjad.Clef(converted_clef)
                 first_leaf = abjad.select(voice).leaves()[0]
                 abjad.attach(clef, first_leaf)
-            self._add_ottavas(voice)
+                self._add_ottavas(voice, clef.name)
         else:
-            print("CLEF INPUT REQUIRED: got NONE type")
-            pass
+            clef = abjad.Clef("treble")
+            first_leaf = abjad.select(voice).leaves()[0]
+            abjad.attach(clef, first_leaf)
+            self._add_ottavas(voice, clef.name)
 
-    def _add_ottavas(self, voice):  # needs to consider what clef is active
-        if self.ottava_shelf is not None:
-            shelf = self.ottava_shelf
-            for run in abjad.select(voice).runs():
-                for tie in abjad.select(run).logical_ties(pitched=True):
-                    for pitch in abjad.select(tie[0]).pitches():
-                        if pitch > shelf:
-                            abjad.ottava(
-                                tie, start_ottava=abjad.Ottava(n=self.ottava_number)
-                            )
+    def _add_ottavas(self, voice, active_clef):
+        if self.add_ottavas is True:
+            current_clef = active_clef
+            if self.ottava_shelf is not None:
+                shelf = self.ottava_shelf
+                if self.extend_in_direction == "down":
+                    for run in abjad.select(voice).runs():
+                        for tie in abjad.select(run).logical_ties(pitched=True):
+                            for pitch in abjad.inspect(tie[0]).pitches():
+                                if pitch < shelf:
+                                    abjad.ottava(
+                                        tie, start_ottava=abjad.Ottava(n=-1)
+                                    )
+                else:
+                    for run in abjad.select(voice).runs():
+                        for tie in abjad.select(run).logical_ties(pitched=True):
+                            for pitch in abjad.inspect(tie[0]).pitches():
+                                if pitch > shelf:
+                                    abjad.ottava(
+                                        tie, start_ottava=abjad.Ottava(n=1)
+                                    )
+            else:
+                shelf = self._extended_range_ottavas(current_clef)
+                if self.extend_in_direction == "down":
+                    for run in abjad.select(voice).runs():
+                        for tie in abjad.select(run).logical_ties(pitched=True):
+                            for pitch in abjad.inspect(tie[0]).pitches():
+                                if pitch < shelf:
+                                    abjad.ottava(
+                                        tie, start_ottava=abjad.Ottava(n=-1)
+                                    )
+                else:
+                    for run in abjad.select(voice).runs():
+                        for tie in abjad.select(run).logical_ties(pitched=True):
+                            for pitch in abjad.inspect(tie[0]).pitches():
+                                if pitch > shelf:
+                                    abjad.ottava(
+                                        tie, start_ottava=abjad.Ottava(n=1)
+                                    )
         else:
-            shelf = self._extended_range_ottavas(self.clef)
-            for run in abjad.select(voice).runs():
-                for tie in abjad.select(run).logical_ties(pitched=True):
-                    for pitch in abjad.inspect(tie[0]).pitches():
-                        if pitch > shelf:
-                            abjad.ottava(
-                                tie, start_ottava=abjad.Ottava(n=self.ottava_number)
-                            )
+            pass
