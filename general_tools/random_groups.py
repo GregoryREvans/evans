e87b4ad98697a7c02962cc463807bb4125d469db
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-08-25 18:42:55 -0500

    Added random grouper.

---
+++ general_tools/random_groups.py
@@ -0,0 +1,17 @@
+from random import shuffle
+
+
+items = ["item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", ]
+shuffle(items)
+def subgroup(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+groups = list(subgroup(items, 3))
+for group in groups:
+    print(group)
+
+###sample result###
+# ['item7', 'item8', 'item3']
+# ['item4', 'item1', 'item2']
+# ['item6', 'item5']
