8812c428ace998cbcf37688f0197320d5d687270
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-09-09 11:13:47 -0500

    Initializer updates.

--- evans/general_tools/flatten.py
+++ evans/general_tools/flatten.py
@@ -1,6 +1,8 @@
 def flatten(lst):
     out = []
     for i in lst:
-        for x in i:
-            out.extend([x])
+        if isinstance(i, int):
+            out.append(i)
+        else:
+            out.extend(flatten(i))
     return out
