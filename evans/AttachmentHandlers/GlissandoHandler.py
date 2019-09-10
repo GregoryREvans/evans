d3253fd74982787708f5acc83440200808203ffb
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-09-10 09:22:12 -0500

    Adding boolean vectors.

--- evans/AttachmentHandlers/GlissandoHandler.py
+++ evans/AttachmentHandlers/GlissandoHandler.py
@@ -1,10 +1,16 @@
 import abjad
+from evans.AttachmentHandlers.CyclicList import CyclicList
 
 
 class GlissandoHandler:
-    def __init__(self, glissando_style=None, line_style=None):
+    def __init__(
+        self, glissando_style=None, line_style=None, boolean_vector=[0], continuous=True
+    ):
         self.glissando_style = glissando_style
         self.line_style = line_style
+        self._count = -1
+        self.continuous = continuous
+        self.boolean_vector = CyclicList(boolean_vector, self.continuous, self._count)
 
     def __call__(self, selections):
         self.add_glissando(selections)
@@ -14,43 +20,77 @@ class GlissandoHandler:
         if self.glissando_style == "hide_middle_note_heads":
             if self.line_style != None:
                 for run in runs:
-                    if len(run) > 1:
-                        abjad.glissando(
-                            run[:],
-                            abjad.tweak(self.line_style).style,
-                            hide_middle_note_heads=True,
-                        )
+                    if self.boolean_vector(r=1)[0] is 0:
+                        if len(run) > 1:
+                            abjad.glissando(
+                                run[:],
+                                abjad.tweak(self.line_style).style,
+                                hide_middle_note_heads=True,
+                            )
+                        else:
+                            continue
+                    else:
+                        continue
             else:
                 for run in runs:
-                    if len(run) > 1:
-                        abjad.glissando(run[:], hide_middle_note_heads=True)
+                    if self.boolean_vector(r=1)[0] is 0:
+                        if len(run) > 1:
+                            abjad.glissando(run[:], hide_middle_note_heads=True)
+                        else:
+                            continue
+                    else:
+                        continue
         elif self.glissando_style == "hide_middle_stems":
             if self.line_style != None:
                 for run in runs:
-                    if len(run) > 1:
-                        abjad.glissando(
-                            run[:],
-                            abjad.tweak(self.line_style).style,
-                            hide_middle_note_heads=True,
-                            hide_middle_stems=True,
-                        )
+                    if self.boolean_vector(r=1)[0] is 0:
+                        if len(run) > 1:
+                            abjad.glissando(
+                                run[:],
+                                abjad.tweak(self.line_style).style,
+                                hide_middle_note_heads=True,
+                                hide_middle_stems=True,
+                            )
+                        else:
+                            continue
+                    else:
+                        continue
             else:
                 for run in runs:
-                    if len(run) > 1:
-                        abjad.glissando(
-                            run[:], hide_middle_note_heads=True, hide_middle_stems=True
-                        )
+                    if self.boolean_vector(r=1)[0] is 0:
+                        if len(run) > 1:
+                            abjad.glissando(
+                                run[:],
+                                hide_middle_note_heads=True,
+                                hide_middle_stems=True,
+                            )
+                        else:
+                            continue
+                    else:
+                        continue
         else:
             if self.line_style != None:
                 for run in runs:
-                    if len(run) > 1:
-                        abjad.glissando(
-                            run[:],
-                            abjad.tweak(self.line_style).style,
-                            allow_repeats=True,
-                            allow_ties=False,
-                        )
+                    if self.boolean_vector(r=1)[0] is 0:
+                        if len(run) > 1:
+                            abjad.glissando(
+                                run[:],
+                                abjad.tweak(self.line_style).style,
+                                allow_repeats=True,
+                                allow_ties=False,
+                            )
+                        else:
+                            continue
+                    else:
+                        continue
             else:
                 for run in runs:
-                    if len(run) > 1:
-                        abjad.glissando(run[:], allow_repeats=True, allow_ties=False)
+                    if self.boolean_vector(r=1)[0] is 0:
+                        if len(run) > 1:
+                            abjad.glissando(
+                                run[:], allow_repeats=True, allow_ties=False
+                            )
+                        else:
+                            continue
+                    else:
+                        continue
--- evans/AttachmentHandlers/SlurHandler.py
+++ evans/AttachmentHandlers/SlurHandler.py
@@ -1,18 +1,28 @@
 import abjad
+from evans.AttachmentHandlers.CyclicList import CyclicList
 
 
 class SlurHandler:
-    def __init__(self, slurs=None):
-        self.slurs = slurs
+    def __init__(self, apply_slur_to="runs", boolean_vector=[0], continuous=True):
+        self.apply_slur_to = apply_slur_to
+        self._count = -1
+        self.continuous = continuous
+        self.boolean_vector = CyclicList(boolean_vector, self.continuous, self._count)
 
     def __call__(self, selections):
         self.add_slurs(selections)
 
     def add_slurs(self, selections):
         if self.slurs == "selections":
-            abjad.slur(selections[:])
+            if self.boolean_vector(r=1)[0] is 0:
+                abjad.slur(selections[:])
+            else:
+                continue
         elif self.slurs == "runs":
             for run in abjad.select(selections).runs():
-                abjad.slur(run[:])
+                if self.boolean_vector(r=1)[0] is 0:
+                    abjad.slur(run[:])
+                else:
+                    continue
         else:
             pass
