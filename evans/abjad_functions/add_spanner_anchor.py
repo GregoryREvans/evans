de5e034dfff820ec299cca1bd8b26ad555d54853
GregoryREvans <gregoryrowlandevans@gmail.com>
2019-12-21 13:14:39 -0600

    Spanner anchor maker class.

--- evans/abjad_functions/add_spanner_anchor.py
+++ evans/abjad_functions/add_spanner_anchor.py
@@ -1,97 +1,94 @@
 import abjad
 
 
-def _calc_anchor(leaf, anchor_dur=None, anchor_leaf=None):
-    if anchor_leaf is not None:
-        anchor_dur = anchor_leaf.written_duration
-        durs = [leaf.written_duration]
-    else:
-        durs = [leaf.written_duration, anchor_dur]
-    container = abjad.Container()
-    pre_mult = leaf.written_duration - anchor_dur
-    mult = pre_mult / leaf.written_duration
-    if isinstance(leaf, abjad.Chord):
-        pitches = [abjad.NamedPitch(_) for _ in abjad.inspect(leaf).pitches()]
-    elif isinstance(leaf, abjad.Note):
-        pitches = [abjad.NamedPitch(_) for _ in abjad.inspect(leaf).pitches()]
-        pitches = pitches[0]
-    else:
-        pass
-    maker = abjad.LeafMaker()
-    new_leaves = [l for l in maker([pitches], durs)]
-    new_leaf = new_leaves[0]
-    indicators = abjad.inspect(leaf).indicators()
-    for indicator in indicators:
-        abjad.attach(indicator, new_leaf)
-    new_leaf.multiplier = mult
-    leaf.multiplier = mult
-    if anchor_leaf is not None:
-        anchor = anchor_leaf
-        l = [_ for _ in abjad.inspect(leaf).pitches()]
-        anchor.written_pitch = l[0]
-    else:
-        anchor = new_leaves[1]
-    abjad.attach(abjad.LilyPondLiteral(r"""\abjad-invisible-music""", format_slot="before"), anchor)
-    abjad.annotate(anchor, "type", "spanner anchor")
-    container.append(new_leaf)
-    container.append(anchor)
-    return container
+class AddSpannerAnchor:
+    def __init__(
+        self,
+        leaf,
+        anchor_dur=None,
+        anchor_leaf=None,
+    ):
+        self.leaf = leaf
+        self.anchor_dur = anchor_dur
+        self.anchor_leaf = anchor_leaf
 
-def _add_anchor(leaf, anchor_dur=None, anchor_leaf=None):
-    abjad.mutate(leaf).replace(_calc_anchor(leaf, anchor_dur, anchor_leaf))
-
-def add_spanner_anchor(leaf, anchor_dur=None, anchor_leaf=None):
-    if abjad.inspect(leaf).leaf(1) is not None:
-        next_leaf = abjad.inspect(leaf).leaf(1)
-        if abjad.inspect(next_leaf).annotation("type") != "spanner anchor":
-            _add_anchor(leaf, anchor_dur, anchor_leaf)
+    def calc_anchor(self):
+        if self.anchor_leaf is not None:
+            self.anchor_dur = self.anchor_leaf.written_duration
+            durs = [self.leaf.written_duration]
+        else:
+            durs = [self.leaf.written_duration, self.anchor_dur]
+        container = abjad.Container()
+        pre_mult = self.leaf.written_duration - self.anchor_dur
+        mult = pre_mult / self.leaf.written_duration
+        if isinstance(self.leaf, abjad.Chord):
+            pitches = [abjad.NamedPitch(_) for _ in abjad.inspect(self.leaf).pitches()]
+        elif isinstance(self.leaf, abjad.Note):
+            pitches = [abjad.NamedPitch(_) for _ in abjad.inspect(self.leaf).pitches()]
+            pitches = pitches[0]
         else:
             pass
-    else:
-        _add_anchor(leaf, anchor_dur, anchor_leaf)
+        maker = abjad.LeafMaker()
+        new_leaves = [l for l in maker([pitches], durs)]
+        new_leaf = new_leaves[0]
+        indicators = abjad.inspect(self.leaf).indicators()
+        for indicator in indicators:
+            abjad.attach(indicator, new_leaf)
+        new_leaf.multiplier = mult
+        self.leaf.multiplier = mult
+        if self.anchor_leaf is not None:
+            anchor = self.anchor_leaf
+            l = [_ for _ in abjad.inspect(self.leaf).pitches()]
+            anchor.written_pitch = l[0]
+        else:
+            anchor = new_leaves[1]
+        abjad.attach(abjad.LilyPondLiteral(r"""\abjad-invisible-music""", format_slot="before"), anchor)
+        abjad.annotate(anchor, "type", "spanner anchor")
+        container.append(new_leaf)
+        container.append(anchor)
+        return container
+
+    def add_anchor(self):
+        abjad.mutate(self.leaf).replace(self.calc_anchor()[:])
+
+    def add_spanner_anchor(self):
+        if abjad.inspect(self.leaf).leaf(1) is not None:
+            next_leaf = abjad.inspect(self.leaf).leaf(1)
+            if abjad.inspect(next_leaf).annotation("type") != "spanner anchor":
+                self.add_anchor()
+            else:
+                pass
+        else:
+            self.add_anchor()
 
 # #DEMO 1#
 # staff = abjad.Staff("cs'4")
-# add_spanner_anchor(leaf=staff[0], anchor_dur=abjad.Duration(1, 16))
-# add_spanner_anchor(leaf=staff[0], anchor_dur=abjad.Duration(1, 16))
+# anchor_maker1 = AddSpannerAnchor(leaf=staff[0], anchor_dur=abjad.Duration(1, 16))
+# anchor_maker1.add_spanner_anchor()
+# anchor_maker2 = AddSpannerAnchor(leaf=staff[0], anchor_dur=abjad.Duration(1, 16))
+# anchor_maker2.add_spanner_anchor()
 # abjad.f(staff)
 #
 # #DEMO 2#
 # staff = abjad.Staff("cs'4")
 # a_leaf = abjad.Note("c'16")
-# add_spanner_anchor(leaf=staff[0], anchor_leaf=a_leaf)
+# maker = AddSpannerAnchor(leaf=staff[0], anchor_leaf=a_leaf)
+# maker.add_spanner_anchor()
 # abjad.f(staff)
 #
 #DEMO 3#
 staff = abjad.Staff("c'4 c'4 c'4 r4 c'4 c'4 r8 c'8 c'4 r1")
 selections = abjad.select(staff[:])
+new_selections = abjad.Selection()
 for run in abjad.select(selections).runs():
-    print("RUN")
-    print(run)
     ties = abjad.select(run).logical_ties()
-    print("FINAL LEAF PARENTAGE")
-    print(abjad.inspect(ties[-1][-1]).parentage())
-    print("FOLLOWING LEAF")
-    print(abjad.inspect(ties[-1][-1]).leaf(1))
-    add_spanner_anchor(leaf=ties[-1][-1], anchor_dur=abjad.Duration(1, 16))
-
-for run in abjad.select(selections).runs():
-    print("POST ANCHOR RUN")
-    print(run)
-    # print("POST ANCHOR TIES")
-    # predicate = abjad.inspect().annotation("type")
-    # ties = abjad.select(run).logical_ties().filter(predicate)
-    # print(ties)
-    print("POST ANCHOR FOLLOWING LEAF")
-    print(abjad.inspect(ties[-1][-1]).leaf(1))
-    print("POST ANCHOR FINAL LEAF PARENTAGE")
-    print(abjad.inspect(ties[-1][-1]).parentage())
-    print("")
-
-abjad.f(staff)
-
-for run in abjad.select(staff).runs():
-    print(run)
-
-#ADDING SPANNER ANCHOR REMOVES LEAVES FROM SELECTION AND DOES NOT REPLACE
-#FIND A WAY TO RESELECT?
+    maker = AddSpannerAnchor(leaf=ties[-1][-1], anchor_dur=abjad.Duration(1, 16))
+    maker.add_spanner_anchor()
+    sel = abjad.select(run)
+    tail = abjad.select(maker.calc_anchor()[-1])
+    print(sel)
+    new_selections + sel
+    print(new_selections)
+    new_selections + tail
+    print(new_selections)
+#DOESN'T WORK
