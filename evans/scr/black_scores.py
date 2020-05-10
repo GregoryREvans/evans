80e5a854496d4304563255984a316b5ad63880dd
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-05-10 10:15:49 -0500

    Beautifying.

--- evans/scr/black_scores.py
+++ evans/scr/black_scores.py
@@ -2,9 +2,7 @@ import os
 from return_directories import return_directories
 
 
-print("Formatting Scores ...")
-
-
 def black_scores():
+    print("Formatting Scores ...")
     for score in return_directories():
         os.system(f"black {score}")
