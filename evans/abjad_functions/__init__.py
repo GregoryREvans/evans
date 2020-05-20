f250c201e8b6e2e35f5c7f51ceb5b923a8d02d0c
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-05-20 11:33:00 -0400

    Scripts.

--- evans/abjad_functions/__init__.py
+++ evans/abjad_functions/__init__.py
@@ -8,5 +8,16 @@ from .metric_modulation import metric_modulation
 from .pitch_rounding import *
 from .pitch_warp import pitch_warp
 from .rtm import *
-from .timespan_functions import TimespanSpecifier
+from .timespan_functions import (
+    SilentTimespan,
+    TimespanSpecifier,
+    make_split_list,
+    collect_offsets,
+    make_showable_list,
+    add_silent_timespans,
+    add_silences_to_timespan_lists,
+    add_silences_to_timespan_dict,
+    talea_timespans,
+    )
+from .TimespanMaker import TimespanMaker
 from .timespan_human_keys import human_sorted_keys, to_digit
